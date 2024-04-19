8c8bd9d08a8cf23713937159d4d0af9d01c8f32e
汪明杰 <1209497940@qq.com>
2024-04-20 01:26:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 文档说明
+
+本技术文档采用VuePress技术栈（https://v2.vuepress.vuejs.org/）
+
+需要node版本：18.16.0+
+
+推荐node版本：20+
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { webpackBundler } from '@vuepress/bundler-webpack'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+
+  title: 'CoVscode',
+  description: 'CoVscode使用说明',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: webpackBundler(),
+  base: 'https://github.com/cscw-and-se/coVscode-2024/docs/',
+})
