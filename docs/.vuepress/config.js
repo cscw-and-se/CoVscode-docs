d6393290d535a22f1002f6e5a5be4dc7bd4e3a86
汪明杰 <1209497940@qq.com>
2024-04-20 01:43:59 +0800

    add description

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ export default defineUserConfig({
   lang: 'zh-CN',
 
   title: 'CoVscode',
-  description: 'CoVscode使用说明',
+  description: 'CoVscode使用说明，以及开发过程中的技术细节等',
 
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
